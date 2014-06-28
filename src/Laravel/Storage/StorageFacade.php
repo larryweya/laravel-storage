fcd35c82b66a23625b0590b679fec523f9633c54
Larry Weya <larryweya@gmail.com>
2014-06-28 13:48:32 +0300

    Initial commit

---
+++ src/Laravel/Storage/StorageFacade.php
@@ -0,0 +1,14 @@
+<?php namespace Laravel\Storage;
+
+use Illuminate\Support\Facades\Facade;
+
+class StorageFacade extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'storage'; }
+
+}
---
+++ src/Laravel/Storage/StorageInterface.php
@@ -0,0 +1,30 @@
+<?php namespace Laravel\Storage;
+
+interface StorageInterface {
+
+    /**
+     * Store the file at src to dst
+     *
+     * @param  string  $src
+     * @param  array   $dst
+     * @return string
+     */
+    public function put($src, $dst);
+
+    /**
+     * Get the url to the file at path
+     *
+     * @param  string  $path
+     * @return string
+     */
+    public function getURL($path);
+
+    /**
+     * Delete the file at path
+     *
+     * @param  string  $path
+     * @return bool
+     */
+    public function delete($path);
+
+}
